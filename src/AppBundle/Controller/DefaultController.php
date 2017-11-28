039814ae3a98001b080ee16812f6613153cd62f3
m4rthiz <m4rthiz@gmail.com>
2017-11-28 14:16:34 +0100

    add style

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -18,4 +18,95 @@ class DefaultController extends Controller
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction()
+    {
+        return $this->render(':default/pages:contact.html.twig');
+    }
+
+    /**
+     * @Route("/action", name="action")
+     */
+    public function actionAction()
+    {
+        return $this->render(':default/pages:action.html.twig');
+    }
+
+    /**
+     * @Route("/agenda", name="agenda")
+     */
+    public function agendaAction()
+    {
+        return $this->render(':default/pages:agenda.html.twig');
+    }
+
+
+    /**
+     * @Route("/credits", name="credits")
+     */
+    public function creditsAction()
+    {
+        return $this->render(':default/pages:credits.html.twig');
+    }
+
+
+    /**
+     * @Route("/equipe", name="equipe")
+     */
+    public function equipeAction()
+    {
+        return $this->render(':default/pages:equipe.html.twig');
+    }
+
+    /**
+     * @Route("/mentions", name="mentions")
+     */
+    public function mentionsAction()
+    {
+        return $this->render(':default/pages:mentions.html.twig');
+    }
+
+    /**
+     * @Route("/partenaires", name="partenaires")
+     */
+    public function partenairesAction()
+    {
+        return $this->render(':default/pages:partenaires.html.twig');
+    }
+
+    /**
+     * @Route("/photos", name="photos")
+     */
+    public function photosAction()
+    {
+        return $this->render(':default/pages:photos.html.twig');
+    }
+
+    /**
+     * @Route("/spectacles", name="spectacles")
+     */
+    public function spectaclesAction()
+    {
+        return $this->render(':default/pages:spectacles.html.twig');
+    }
+
+    /**
+     * @Route("/video", name="video")
+     */
+    public function videoAction()
+    {
+        return $this->render(':default/pages:video.html.twig');
+    }
+
+
+    /**
+     * @Route("/ecoutes", name="ecoutes")
+     */
+    public function ecoutesAction()
+    {
+        return $this->render(':default/pages:ecoutes.html.twig');
+    }
 }
--- web/js/main.js
+++ web/js/main.js
@@ -1,80 +1,17 @@
-/*
-$(window).on('scroll', function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 20) {
-        $('#header').stop().animate({height: "170"},5);
-
-        $('#titleHeader').stop().fadeOut(50);
-    }
-    else {
-        $('#header').stop().animate({height: "400"},5);
-
-        $('#titleHeader').stop().fadeIn(50);
-    }
-});
-*/
-
 $(document).ready(function () {
+    $(".logoSmall").hide();
 
-    $("textarea").attr("maxlength", 1200);
+    /**************FIXED NAV**************/
+    $(window).scroll(function () {
 
-    //Swiper
-    var swiper = new Swiper ('.swiper-container', {
-        // Optional parameters
-        loop: true,
-        spaceBetween: 30,
-        hashNavigation: {
-            watchState: true
-        },
+        var height = $(this).scrollTop();
 
-        // If we need pagination
-        pagination: {
-            el: '.swiper-pagination',
-            clickable: true
-        },
-
-        // Navigation arrows
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-
-        // And if we need scrollbar
-        scrollbar:{
-            el: '.swiper-scrollbar'
+        if (height >= 215) {
+            $('nav').addClass('fixed');
+            $(".logoSmall").show();
+        } else {
+            $('nav').removeClass('fixed');
+            $(".logoSmall").hide();
         }
     });
-
-//NavBar
-    /* $("#navbar").sticky({topSpacing: 150, zIndex: 6});*/
-    /*$("#search").sticky({topSpacing: 0, zIndex: 6,getWidthFrom: "500px", center: true});*/
-
-    //CAROUSSEL
-    $('#myCarousel').carousel({
-        interval: 5000
-    });
-
-    //Limit Checkbox
-    var cb = document.querySelectorAll("[class=check-limit]");
-    var i = 0,
-        l = cb.length;
-    for (; i < l; i++)
-        cb[i].addEventListener("change", function () {
-            if (document.querySelectorAll(":checked").length > 3)
-                this.checked = false;
-        }, false);
-
-    //Modal de connection
-    $('#formConnect').on('shown.bs.modal', function () {
-        $('#myInput').focus()
-    });
 });
-
-function showImg(idimg, img) {
-    document.getElementById(idimg).setAttribute('src', img);
-}
-
-function hideImg(idimg, img) {
-    document.getElementById(idimg).setAttribute('src', img);
-}
-
