f515cdaba56285f5cdc923dd62865fa33cf063fd
m4rthiz <m4rthiz@gmail.com>
2017-12-05 17:59:26 +0100

    style for agenda and equipe files

--- web/js/main.js
+++ web/js/main.js
@@ -8,12 +8,45 @@ $(document).ready(function () {
 
         if (height >= 215) {
             $('nav').addClass('fixed');
-            $('.article').css('margin-top', '60px')
+            $('.content').css('margin-top', '65px')
             $(".logoSmall").show();
         } else {
             $('nav').removeClass('fixed');
-            $('.article').css('margin-top', '0px')
+            $('.content').css('margin-top', '0px')
             $(".logoSmall").hide();
         }
     });
 });
+/*
+rubik = {
+    misc:{
+        navbar_menu_visible: 0
+    },
+initAnimationsCheck: function(){
+
+    $('[class*="add-animation"]').each(function(){
+        offset_diff = 30;
+        if($(this).hasClass('title')){
+            offset_diff = 110;
+        }
+
+        var waypoints = $(this).waypoint(function(direction) {
+            if(direction == 'down'){
+                $(this.element).addClass('animate');
+            } else {
+                $(this.element).removeClass('animate');
+            }
+        }, {
+            offset: window_height - offset_diff
+        });
+    });
+
+},
+.animated {
+  -webkit-animation-duration: 5s;
+  animation-duration: 5s;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+}
+
+*/
